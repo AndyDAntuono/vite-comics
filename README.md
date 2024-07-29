6392047f13719966fcdfc5fc589e9bc14968ede4
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-07-30 00:04:01 +0200

    [UPD] updated AppHeader.vue and readme.md files

--- README.md
+++ README.md
@@ -21,3 +21,8 @@ Creare un componente aggiuntivo per gestire la fascia azzurra con le icone.
     <style lang="scss">
     @use './styles/generals.scss';
     </style>
+- creo il file AppHeader.vue;
+- nel file AppHeader.vue, sezione script, inserisco un array di oggetti rappresentante la barra di navigazione;
+- nel file AppHeader.vue, sezione template creo una struttura rigida contenente il contenuto dell'header
+- sempre nella sezione template applico un v-for per tutte le voci della barra di navigazione;
+- nel file AppHeader, sezione style, creo delle classi peronalizzate per la barra di navigazione. In particolare imposto che la voce attiva del menu si colori di blu ed appaio un effetto sottolineato (ho usato un border bottom) del medesimo colore.
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,3 +1,63 @@
+<script>
+export default {
+    data() {
+        return {
+            links: [
+                {
+                    label: 'CHARACTERS',
+                    url: '#',
+                    current: false
+                },
+                {
+                    label: 'COMICS',
+                    url: '#',
+                    current: true
+                },
+                {
+                    label: 'MOVIES',
+                    url: '#',
+                    current: false
+                },
+                {
+                    label: 'TV',
+                    url: '#',
+                    current: false
+                },
+                {
+                    label: 'GAMES',
+                    url: '#',
+                    current: false
+                },
+                {
+                    label: 'COLLECTIBLES',
+                    url: '#',
+                    current: false
+                },
+                {
+                    label: 'VIDEOS',
+                    url: '#',
+                    current: false
+                },
+                {
+                    label: 'FANS',
+                    url: '#',
+                    current: false
+                },
+                {
+                    label: 'NEWS',
+                    url: '#',
+                    current: false
+                },
+                {
+                    label: 'SHOP',
+                    url: '#',
+                    current: false
+                },
+            ]
+        }
+    }
+}
+</script>
 <template lang="">
     <header>
         <div class="container">
@@ -9,35 +69,8 @@
                             <div class="container-fluid">
                                 <div class="collapse navbar-collapse">
                                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                        <li class="nav-item">
-                                          <a class="nav-link" href="#">CHARACTERS</a>
-                                        </li>
-                                        <li class="nav-item active">
-                                          <a class="nav-link" href="#">COMICS</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="#">MOVIES</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="#">TV</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="#">GAMES</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="#">COLLECTIBLES</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="#">VIDEOS</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="#">FANS</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="#">NEWS</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="#">SHOP</a>
+                                        <li v-for="link, index in links" :key="index" :class="['nav-item', link.current ? 'active' : '']">
+                                          <a class="nav-link" :href="link.url">{{ link.label }}</a>
                                         </li>
                                       </ul>
                                 </div>
@@ -45,16 +78,16 @@
                         </nav>
                         <!--
                         <ul class="list-unstyled d-flex">
-                            <li class="m1">CHARACTERS</li>
-                            <li>COMICS</li>
-                            <li>MOVIES</li>
-                            <li>TV</li>
-                            <li>GAMES</li>
-                            <li>COLLECTIBLES</li>
-                            <li>VIDEOS</li>
-                            <li>FANS</li>
-                            <li>NEWS</li>
-                            <li>SHOP</li>
+                            <li class="m1">CHARACTERS</li> 1
+                            <li>COMICS</li> 2
+                            <li>MOVIES</li> 3
+                            <li>TV</li> 4
+                            <li>GAMES</li> 5
+                            <li>COLLECTIBLES</li> 6
+                            <li>VIDEOS</li> 7
+                            <li>FANS</li> 8
+                            <li>NEWS</li> 9
+                            <li>SHOP</li> 10
                         </ul>
                     -->
                     </div>
@@ -64,7 +97,7 @@
     </header>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .nav-link {
     font-weight: bold;
     font-size: 20px;
@@ -72,5 +105,6 @@
 
 .nav-item.active .nav-link {
     color: blue;
+    border-bottom: 3px solid blue;
 }
 </style>
