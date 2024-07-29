053d2a626f12e342798d1eb3a48c1b21400be00e
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-07-29 20:43:07 +0200

    [UPD] updated AppHeader.vue

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,66 @@
 <template laang="">
     <header>
-        <img src="../assets/img/dc-logo.png"
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="d-flex justify-content-between align-intems-center py-3 bg-white">
+                        <img src="../assets/img/dc-logo.png" />
+                        <nav class="navbar navbar-expand-lg">
+                            <div class="container-fluid">
+                                <div class="collapse navbar-collapse">
+                                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                        <li class="nav-item">
+                                          <a class="nav-link active" href="#">CHARACTERS</a>
+                                        </li>
+                                        <li class="nav-item">
+                                          <a class="nav-link" href="#">COMICS</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">MOVIES</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">TV</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">GAMES</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">COLLECTIBLES</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">VIDEOS</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">FANS</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">NEWS</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">SHOP</a>
+                                        </li>
+                                      </ul>
+                                </div>
+                            </div>
+                        </nav>
+                        <!--
+                        <ul class="list-unstyled d-flex">
+                            <li class="m1">CHARACTERS</li>
+                            <li>COMICS</li>
+                            <li>MOVIES</li>
+                            <li>TV</li>
+                            <li>GAMES</li>
+                            <li>COLLECTIBLES</li>
+                            <li>VIDEOS</li>
+                            <li>FANS</li>
+                            <li>NEWS</li>
+                            <li>SHOP</li>
+                        </ul>
+                    -->
+                    </div>
+                </div>
+            </div>
+        </div>
     </header>
 </template>
 <script>
