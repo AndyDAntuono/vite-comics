74de023b6d6b26beafa473d755624a7a7e50d9d0
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-07-30 01:54:45 +0200

    [UPD] updated AppFooter.vue

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,11 +3,88 @@ export default {
     
 }
 </script>
-<template lang="">
-    <div>
-        
-    </div>
+
+<template>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="d-flex justify-content-between align-items-center bg-primary text-white">
+                        <div class="icon_and-text">
+                            <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                            <p>DIGITAL COMICS</p>
+                        </div>
+                        <div class="icon_and-text">
+                            <img src="../assets/img/buy-comics-merchandise.png" alt="">
+                            <p>DC MERCHANDISE</p>
+                        </div>
+                        <div class="icon_and-text">
+                            <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+                            <p>SUBSCRIPTIONS</p>
+                        </div>
+                        <div class="icon_and-text">
+                            <img src="../assets/img/buy-comics-shop-locator.png" alt="">
+                            <p>COMIC SHOP LOCATOR</p>
+                        </div>
+                        <div class="icon_and-text">
+                            <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+                            <p>DC POWER VISA</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row bg-footer">
+                <div class="col-12">
+                    <div class="d-flex flex-row mb-3 text-white">
+                        <div class="p-2">
+                            <ul>Qualcosa
+                                <li>lorem</li>
+                            </ul>
+                            <ul>Qualcosa
+                                <li>lorem</li>
+                            </ul>
+                        </div>
+                        <div class="p-2">
+                            <ul>Qualcosa
+                                <li>lorem</li>
+                            </ul>
+                        </div>
+                        <div class="p-2">
+                            <ul>Qualcosa
+                                <li>lorem</li>
+                            </ul>
+                        </div>
+                      </div>
+                </div>
+            </div>
+            <div class="row bg-secondary">
+                <div class="col-12">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="sign-up_button m-1">SIGN UP</div>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="follow-us">
+                                follow us
+                            </div>
+                            <img src="../assets/img/footer-facebook.png" alt="">
+                            <img src="../assets/img/footer-twitter.png" alt="">
+                            <img src="../assets/img/footer-youtube.png" alt="">
+                            <img src="../assets/img/footer-pinterest.png" alt="">
+                            <img src="../assets/img/footer-periscope.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
-<style lang="">
-    
+
+<style lang="scss" scoped>
+.bg-footer {
+    background-image: url('../assets/img/footer-bg.jpg');
+    background-size: cover;
+}
+.sign-up_button {
+    border: 1px solid blue;
+    color: white;
+}
 </style>
